85ece67903d9e6a426013a4c58c7040140724117
Maple <maple.shao@everbridge.com>
2018-07-23 19:28:59 +0800

    add ali hotfix.

--- app/src/main/java/com/maple/recordwav/WavApp.java
+++ app/src/main/java/com/maple/recordwav/WavApp.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.os.Environment;
 import android.util.Log;
 
+import com.taobao.sophix.PatchStatus;
+import com.taobao.sophix.SophixManager;
+import com.taobao.sophix.listener.PatchLoadStatusListener;
+
 import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
@@ -33,6 +37,7 @@ public class WavApp extends Application {
 
         ACRA.init(this);
         initPath();
+        addHotfix();
     }
 
     /**
@@ -41,7 +46,7 @@ public class WavApp extends Application {
     private void initPath() {
         String ROOT = "";// /storage/emulated/0
         if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-//            ROOT = getBaseContext().getFilesDir().getPath();
+            // ROOT = getBaseContext().getFilesDir().getPath();
             ROOT = Environment.getExternalStorageDirectory().getPath();
             Log.e("app", "系统方法：" + ROOT);
         }
@@ -53,6 +58,30 @@ public class WavApp extends Application {
         }
     }
 
+    private void addHotfix() {
+        String appVersion = "";
+        // initialize必须放在attachBaseContext最前面，初始化代码直接写在Application类里面，切勿封装到其他类。
+        SophixManager.getInstance().setContext(this)
+                .setAppVersion(appVersion)
+                .setAesKey(null)
+                .setEnableDebug(true)
+                .setPatchLoadStatusStub(new PatchLoadStatusListener() {
+                    @Override
+                    public void onLoad(int mode, int code, String info, int handlePatchVersion) {
+                        // 补丁加载回调通知
+                        if (code == PatchStatus.CODE_LOAD_SUCCESS) {
+                            // 表明补丁加载成功
+                        } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
+                            // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
+                            // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
+                        } else {
+                            // 其它错误信息, 查看PatchStatus类说明
+                        }
+                    }
+                }).initialize();
+        // queryAndLoadNewPatch不可放在attachBaseContext 中，否则无网络权限，建议放在后面任意时刻，如onCreate中
+        SophixManager.getInstance().queryAndLoadNewPatch();
+    }
 
     public static WavApp app() {
         return app;
