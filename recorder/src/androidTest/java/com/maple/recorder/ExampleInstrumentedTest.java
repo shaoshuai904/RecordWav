8b5a300fe6ca4cd28878b22b0ecb289ebb09558d
MapleShao <939078792@qq.com>
2018-08-16 16:58:45 +0800

    update gradle version.

--- recorder/src/androidTest/java/com/maple/recorder/ExampleInstrumentedTest.java
+++
@@ -1,9 +0,0 @@
-package com.maple.recorder;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleInstrumentedTest {
-}
