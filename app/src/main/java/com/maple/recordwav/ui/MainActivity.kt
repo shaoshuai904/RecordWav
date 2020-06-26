8dcca82ac9a424c58f9ab232e61382f413dd4b1a
MapleShao <939078792@qq.com>
2020-06-26 17:43:33 +0800

    update main page tab.

--- app/src/main/java/com/maple/recordwav/ui/MainActivity.kt
+++ app/src/main/java/com/maple/recordwav/ui/MainActivity.kt
@@ -2,48 +2,57 @@ package com.maple.recordwav.ui
 
 import android.Manifest
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayout
 import com.maple.recordwav.R
+import com.maple.recordwav.databinding.ActivityMainBinding
+import com.maple.recordwav.utils.BottomTabView
 import com.maple.recordwav.utils.FragmentChangeManager
 import com.maple.recordwav.utils.permission.RxPermissions
-import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * @author maple
  * @time 2018/4/8.
  */
 class MainActivity : FragmentActivity() {
-    private lateinit var fgManager: FragmentChangeManager
+    private lateinit var binding: ActivityMainBinding
+    private var tabIndex: Int = 0 // 当前显示页面
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
 
         initView()
         requestPermission()
     }
 
     private fun initView() {
-        val fragmentArray = arrayListOf(RecordPage(), PlayPage(), ParsePage())
-        fgManager = FragmentChangeManager(supportFragmentManager, R.id.fl_content, fragmentArray)
-
-        nav_view.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.nvg_home -> {
-                    fgManager.setCurrentFragment(0)
-                    true
-                }
-                R.id.nvg_play -> {
-                    fgManager.setCurrentFragment(1)
-                    true
-                }
-                R.id.nvg_info -> {
-                    fgManager.setCurrentFragment(2)
-                    true
-                }
-                else -> false
+        val fragmentList = arrayListOf(RecordPage(), PlayPage(), ParsePage())
+        val iconArr = arrayListOf(R.drawable.sel_tab_record_icon, R.drawable.sel_tab_play_icon, R.drawable.sel_tab_parse_icon)
+        val titleArr = arrayListOf(getString(R.string.record), getString(R.string.play), getString(R.string.info))
+        fragmentList.forEachIndexed { index, _ ->
+            val tab: TabLayout.Tab = binding.tlTab.newTab()
+            tab.customView = BottomTabView(this).apply {
+                setIcon(iconArr[index], null)
+                setTitle(titleArr[index])
             }
+            binding.tlTab.addTab(tab)
         }
+        val fgManager = FragmentChangeManager(supportFragmentManager, R.id.fl_content, fragmentList)
+        binding.tlTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                tabIndex = tab.position
+                fgManager.setCurrentFragment(tabIndex)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                (tab?.customView as BottomTabView?)?.setSelectStatus(false)
+            }
+        })
+        binding.tlTab.getTabAt(tabIndex)?.select()
     }
 
     private fun requestPermission() {
---
+++ app/src/main/java/com/maple/recordwav/utils/BottomTabView.kt
@@ -0,0 +1,84 @@
+package com.maple.recordwav.utils
+
+import android.content.Context
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.annotation.DrawableRes
+import androidx.databinding.DataBindingUtil
+import com.maple.recordwav.R
+import com.maple.recordwav.databinding.ItemButtomTabViewBinding
+
+/**
+ * App主页面底部tab view
+ *
+ * @author : shaoshuai27
+ * @date ：2019/10/22
+ */
+class BottomTabView : FrameLayout {
+    private lateinit var binding: ItemButtomTabViewBinding
+    private var iconRes: Int? = null
+    private var animatorRes: Int? = null
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        binding = DataBindingUtil.inflate(
+                LayoutInflater.from(context),
+                R.layout.item_buttom_tab_view, this, true
+        )
+    }
+
+    fun setIcon(@DrawableRes selectedRes: Int, @DrawableRes animatorRes: Int?) {
+        this.iconRes = selectedRes
+        this.animatorRes = animatorRes
+        binding.ivIcon.setImageResource(selectedRes)
+    }
+
+    fun setTitle(title: String?) {
+        binding.tvTitle.text = title
+    }
+
+    fun setSelectStatus(select: Boolean) {
+        if (select && animatorRes != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            try {
+                val drawable = context.getDrawable(animatorRes!!)
+                binding.ivIcon.setImageDrawable(drawable)
+                if (drawable is AnimatedVectorDrawable) {
+                    drawable.registerAnimationCallback(object : Animatable2.AnimationCallback() {
+                        override fun onAnimationStart(drawable: Drawable) {}
+                        override fun onAnimationEnd(drawable: Drawable) {
+                            resetIconRes()
+                        }
+                    })
+                    drawable.start()
+                }
+            } catch (e: Exception) {
+                resetIconRes()
+            }
+        } else {
+            resetIconRes()
+        }
+    }
+
+    // 重置图标状态
+    private fun resetIconRes() {
+        if (iconRes != null) {
+            binding.ivIcon.setImageResource(iconRes!!)
+        }
+    }
+}
