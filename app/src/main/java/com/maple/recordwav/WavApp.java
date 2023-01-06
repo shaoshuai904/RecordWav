f45eee5ea1a8e8300f48606e0045a6764d1d2260
shaoshuai <939078792@qq.com>
2023-01-06 11:49:19 +0800

    升级target 32 和 gradle 7.0

--- app/src/main/java/com/maple/recordwav/WavApp.java
+++ app/src/main/java/com/maple/recordwav/WavApp.java
@@ -3,23 +3,8 @@ package com.maple.recordwav;
 import android.app.Application;
 import android.util.Log;
 
-import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
-
 import java.io.File;
 
-@ReportsCrashes(
-        mode = ReportingInteractionMode.DIALOG,
-        mailTo = "shaoshuai904@gmail.com",
-        resToastText = R.string.crash_toast_text,
-        resDialogText = R.string.crash_dialog_text,
-        resDialogIcon = R.drawable.ic_launcher,
-        resDialogTitle = R.string.crash_dialog_title,
-        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt,
-        // resDialogTheme = R.style.AppTheme_Dialog,
-        resDialogOkToast = R.string.crash_dialog_ok_toast
-)
 public class WavApp extends Application {
     private static WavApp app;
     public static File saveFile;
@@ -30,7 +15,6 @@ public class WavApp extends Application {
         super.onCreate();
 
         initFile();
-        ACRA.init(this);
     }
 
     /**
